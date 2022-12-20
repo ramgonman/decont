45e8cf1aff03b9764f7aae83944adeca42f597cb
Ramon Gonzalez Manzano <ramgonman@hotmail.com>
2022-12-20 15:25:01 +0100

    Add main pipeline.sh script

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,18 +1,23 @@
+#!/bin/bash
 #Download all the files specified in data/filenames
-for url in $(<list_of_urls>) #TODO
+echo "Downloading small-RNA samples ..."
+
+for url in $(cat data/urls) #TODO
 do
     bash scripts/download.sh $url data
 done
 
 # Download the contaminants fasta file, uncompress it, and
 # filter to remove all small nuclear RNAs
-bash scripts/download.sh <contaminants_url> res yes #TODO
-
+echo "Downloading the contaminants fasta file ..."
+echo
+bash scripts/download.sh "https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz" res yes "small nuclear" #TODO
+echo
 # Index the contaminants file
-bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
+bash scripts/index.sh res/contaminants_filtered.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-for sid in $(<list_of_sample_ids>) #TODO
+for sid in $(ls data/*.fastq.gz) #TODO
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
@@ -21,15 +26,30 @@ done
 # cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
 #     -o <trimmed_file> <input_file> > <log_file>
 
+echo "Running cutadapt ..."
+mkdir -p log/cutadapt
+mkdir -p out/trimmed
+
+for sampleid in $(ls out/merged)
+do
+cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
+     -o out/trimmed/$(basename $sampleid .merged.fastq.gz).trimmed.fastq.gz out/merged/$sampleid > log/cutadapt/$(basename $sampleid .merged.fastq.gz).log
+echo "$(date) Summary cutadapt $(basename $sampleid .merged.fastq.gz).log" >> $WD/pipeline.log 
+cat log/cutadapt/$(basename $sampleid .merged.fastq.gz).log | grep -e "with adapter" -e "Total basepairs" >> $WD/pipeline.log
+done
+echo
 # TODO: run STAR for all trimmed files
+echo "Running STAR alignment ..."
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
-    sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-    #    --outReadsUnmapped Fastx --readFilesIn <input_file> \
-    #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
+    sampid=$(basename $fname .trimmed.fastq.gz)       #TODO
+mkdir -p out/star/$sampid
+STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+    --outReadsUnmapped Fastx --readFilesIn $fname \
+    --readFilesCommand gunzip -c --outFileNamePrefix out/star/$sampid/
+echo "$(date) Summary star $sampid" >> $WD/pipeline.log
+cat out/star/$sampid/Log.final.out | grep -e "Uniquely mapped" -e "mapped to multiple loci" -e "mapped to too many loci" >> $WD/pipeline.log
 done 
 
 # TODO: create a log file containing information from cutadapt and star logs
